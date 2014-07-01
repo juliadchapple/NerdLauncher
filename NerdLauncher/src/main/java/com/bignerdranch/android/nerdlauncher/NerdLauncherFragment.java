1664e30a81e4ea6aa61df5d6929c76e973a319b1
Julia Chapple <jchapple@jchapple-macbookpro.roam.corp.google.com>
2014-07-01 12:19:00 -0600

    Initial commit

---
+++ NerdLauncher/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,47 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by jchapple on 5/14/14.
+ */
+public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Log.i(TAG, "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo resolveInfo, ResolveInfo resolveInfo2) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        resolveInfo.loadLabel(pm).toString(),
+                        resolveInfo2.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(), android.R.layout.simple_list_item_1, activities) {
+            //TODO
+        };
+
+    }
+}
